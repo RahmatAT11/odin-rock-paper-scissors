269d0386d7ef4c87bd1a2ab437f1f70769168c76
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2025-03-19 09:24:43 +0700

    feat: Add function to generate computer choice

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World")
+function getComputerChoice() {
+    // Set rng random number between 0, 1, 2
+    // Set computerChoice to empty string
+    // IF rng is equal to 0
+    //   Set computerChoice to "rock"
+    // ELSE IF rng is equal to 1
+    //   Set computerChoice to "paper"
+    // ELSE IF rng is equal to 2
+    //   Set computerChoice to "scissors"
+    // ENDIF
+    // Return that computerChoice
+    const rng = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+
+    if (rng === 0) {
+        computerChoice = "rock";
+    } else if (rng === 1) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice())
