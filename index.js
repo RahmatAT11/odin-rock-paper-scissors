902cadb09e7699a5801818cbc3262d3c133ab06d
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2025-03-25 14:40:25 +0700

    fix: Remove unnecessary comments

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 function getComputerChoice() {
-    // Set rng random number between 0, 1, 2
-    // Set computerChoice to empty string
-    // IF rng is equal to 0 THEN
-    //   Set computerChoice to "rock"
-    // ELSE IF rng is equal to 1 THEN
-    //   Set computerChoice to "paper"
-    // ELSE IF rng is equal to 2 THEN
-    //   Set computerChoice to "scissors"
-    // ENDIF
-    // Return that computerChoice
     const rng = Math.floor(Math.random() * 3);
     let computerChoice = "";
 
@@ -24,9 +14,6 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    // Read an input from human between rock/paper/scissors
-    // Set humanChoice to input from before;
-    // Return humanChoice
     const humanChoice = prompt("Enter your choice (rock/paper/scissors): ");
     return humanChoice;
 }
@@ -36,39 +23,6 @@ function playGame() {
     let computerScore = 0;
 
     function playRound(humanChoice, computerChoice) {
-        // CALL toLowerCase with humanChoice RETURNING humanChoiceNormalized
-        // CALL toLowerCase with computerChoice RETURNING computerChoiceNormalized
-        // IF humanChoiceNormalized is equal to "rock" THEN
-        //   IF computerChoiceNormalized is equal to "rock" THEN
-        //     Display message: "It's a draw!"
-        //   ELSE IF computerChoiceNormalized is equal to "paper" THEN
-        //     Display message: "You lose! Paper beats Rock"
-        //     Increment computerScore
-        //   ELSE IF computerChoiceNormalized is equal to "scissors" THEN
-        //     Display message: "You win! Rock beats Scissors"
-        //     Increment humanScore
-        //   ENDIF
-        // ELSE IF humanChoiceNormalized is equal to "paper" THEN
-        //   IF computerChoiceNormalized is equal to "rock" THEN
-        //     Display message: "You win! Paper beats Rock"
-        //     Increment humanScore
-        //   ELSE IF computerChoiceNormalized is equal to "paper" THEN
-        //     Display message: "It's a draw!"
-        //   ELSE IF computerChoiceNormalized is equal to "scissors" THEN
-        //     Display message: "You lose! Scissors beats Paper"
-        //     Increment computerScore
-        //   ENDIF
-        // ELSE IF humanChoiceNormalized is equal to "scissors" THEN
-        //   IF computerChoiceNormalized is equal to "rock" THEN
-        //     Display message: "You lose! Rock beats Scissors"
-        //     Increment computerScore
-        //   ELSE IF computerChoiceNormalized is equal to "paper" THEN
-        //     Display message: "You win! Scissors beats Paper"
-        //     Increment humanScore
-        //   ELSE IF computerChoiceNormalized is equal to "scissors" THEN
-        //     Display message: "It's a draw!"
-        //   ENDIF
-        // ENDIF
 
         const humanChoiceNormalized = humanChoice.toLowerCase()
         const computerChoiceNormalized = computerChoice.toLowerCase();
@@ -106,24 +60,12 @@ function playGame() {
         }
     }
 
-    // FOR five times iteration
-    //   CALL getHumanChoice RETURNING humanSelection
-    //   CALL getComputerChoice RETURNING computerSelection
-    //   CALL playRound
-
     for (let i = 0; i < 5; i++) {
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
     
         playRound(humanSelection, computerSelection); 
     }
-
-    // IF humanScore is larger than computerScore
-    //   Display message: "You win!"
-    // ELSE
-    //   Display message: "You lose!"
-    // ENDIF
-    // Display player score and computer score
 
     if (humanScore > computerScore) {
         console.log("You win!");
